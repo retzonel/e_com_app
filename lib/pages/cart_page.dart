88f9fce830dca1dd95b8e99fc083497991319936
retzonel <110589356+retzonel@users.noreply.github.com>
2026-04-20 00:47:15 +0100

    add to cart functionalities

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,7 @@
+import 'package:e_com_app/components/cart_item.dart';
+import 'package:e_com_app/models/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,6 +13,31 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Cart"),);
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            Text(
+              "My Cart",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 10),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.getUserCart().length,
+                itemBuilder: (context, index) {
+                  var individualShoe = value.getUserCart()[index];
+
+                  return CartItem(shoe: individualShoe,);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
